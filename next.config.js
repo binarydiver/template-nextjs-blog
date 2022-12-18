4486f84c902274840b7cd4dea55acaba1df6ae80
Jason Park <binarydiver@outlook.com>
2022-12-18 12:11:26 +0900

    setup gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  basePath: isProd ? '/play-nextjs-blog' : '/',
+  // assetPrefix: isProd ? '/play-nextjs-blog/' : '',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
