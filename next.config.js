41461ea0f9213cbe5e7fcd718ecbafe9ab626880
Jason Park <binarydiver@outlook.com>
2023-03-14 21:30:33 +0900

    fix broken images

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   basePath: isProd ? "/template-nextjs-blog" : "",
-  assetPrefix: isProd ? "/template-nextjs-blog/" : "",
   images: {
     unoptimized: true,
   },
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    assetPrefix: isProd ? "/template-nextjs-blog" : "",
+  },
 };
 
 module.exports = nextConfig;
