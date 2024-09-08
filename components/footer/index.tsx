dd41201704fc07260c2edd1d59add3705f5b58e5
Jason Park <jason@binarydiver.com>
2024-09-08 22:38:27 +0900

    replace utterances to giscus as comment solution

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="max-w-5xl mx-auto p-2 text-center">
-      <p>© 2023 Author</p>
+      <p>© 2024 Author</p>
     </footer>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ReactElement, ReactNode } from 'react';
 import Layout from '../components/layout';
-import Comments from '../components/comments';
+import Giscus from '@giscus/react';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,12 +14,31 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout || (page => <Layout>{page}</Layout>);
+  const getLayout =
+    Component.getLayout ||
+    (page => {
+      return (
+        <Layout>
+          {page}
+          {page.props.markdoc && (
+            <Giscus
+              id="comments"
+              repo="binarydiver/template-nextjs-blog"
+              repoId="R_kgDOIogC8w"
+              category="Q&A"
+              categoryId="DIC_kwDOIogC884CiTNx"
+              mapping="pathname"
+              reactionsEnabled="1"
+              emitMetadata="0"
+              inputPosition="bottom"
+              theme="dark"
+              lang="en"
+              loading="lazy"
+            />
+          )}
+        </Layout>
+      );
+    });
 
-  return getLayout(
-    <>
-      <Component {...pageProps} />
-      <Comments />
-    </>
-  );
+  return getLayout(<Component {...pageProps} />);
 }
