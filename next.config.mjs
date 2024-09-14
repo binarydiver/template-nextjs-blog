d540ef92df5c8e151783fb8d2e472d8522a031d6
Jason Park <jason@binarydiver.com>
2024-09-14 20:09:15 +0900

    fix: running time error

--- next.config.mjs
+++ next.config.mjs
@@ -15,23 +15,6 @@ const nextConfig = {
     pageExtensions: ['md', 'mdoc', 'jsx', 'tsx'],
   }),
   trailingSlash: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
