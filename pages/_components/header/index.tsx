f14d51ab94770e17397b62d6e3b3e081a1e94ff7
Jason Park <jason@binarydiver.com>
2024-09-14 15:04:17 +0900

    feat: toggle color-theme

--- pages/_components/header/index.tsx
+++ pages/_components/header/index.tsx
@@ -7,11 +7,34 @@ type HeaderProps = {
 const Header = (props: HeaderProps) => {
   const { title } = props;
 
+  const toggleTheme = () => {
+    const theme = localStorage.getItem('color-theme');
+    if (theme === 'dark') {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('color-theme', 'light');
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('color-theme', 'dark');
+    }
+  };
+
   return (
-    <nav className="border-b flex items-center justify-center p-4">
-      <Link className="no-underline" href={'/'}>
-        <h1 className="leading-normal m-0">{title}</h1>
-      </Link>
+    <nav className="border-b flex items-center p-4">
+      <div className="flex-1"></div>
+      <div className="flex-auto text-center">
+        <Link className="no-underline" href={'/'}>
+          <h1 className="leading-normal m-0">{title}</h1>
+        </Link>
+      </div>
+      <div className="flex-1">
+        <button
+          type="button"
+          className="text-white bg-gray-300 hover:bg-gray-400 focus:ring-gray-500 focus:outline-none focus:ring-2 font-medium rounded-lg text-4xl px-1.5 pb-1.5 dark:bg-gray-100 dark:hover:dark:bg-gray-400 dark:focus:ring-gray-200"
+          onClick={() => toggleTheme()}
+        >
+          🌓︎
+        </button>
+      </div>
     </nav>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,17 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
-    <Html className="dark" lang="en">
+    <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/x-icon" href="./favicon.ico" />
       </Head>
-      <body className="antialiased prose dark:prose-invert max-w-full">
+      <body className="antialiased bg-zinc-100 prose prose-zinc dark:bg-zinc-950 dark:prose-invert max-w-full">
         <Main />
         <NextScript />
+        <Script src="./scripts/theme.js" strategy="beforeInteractive" />
       </body>
     </Html>
   );
