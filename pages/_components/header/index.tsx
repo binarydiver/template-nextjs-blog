575b09067b2d85d151490b24d025165b38c0fc99
Jason Park <jason@binarydiver.com>
2024-09-14 22:41:27 +0900

    feat: add github link to the header

--- pages/_components/header/index.tsx
+++ pages/_components/header/index.tsx
@@ -1,6 +1,7 @@
+import gitHubIcon from '@/public/github-mark-white.svg';
+import rssIcon from '@/public/rss-512.png';
 import Image from 'next/image';
 import Link from 'next/link';
-import rssIcon from '../../../public/rss-512.png';
 
 type HeaderProps = {
   title: string;
@@ -21,30 +22,45 @@ const Header = (props: HeaderProps) => {
   };
 
   return (
-    <nav className="border-b flex items-center p-4">
-      <div className="flex-1"></div>
-      <div className="flex-auto text-center">
-        <Link className="no-underline" href={'/'}>
-          <h1 className="leading-normal m-0">{title}</h1>
-        </Link>
-      </div>
-      <div className="flex flex-1 gap-x-2">
-        <button
-          type="button"
-          className="text-white bg-gray-300 hover:bg-gray-400 focus:ring-gray-500 focus:outline-none focus:ring-2 font-medium rounded-lg text-4xl px-1.5 pb-1 dark:bg-gray-100 dark:hover:dark:bg-gray-400 dark:focus:ring-gray-200"
-          onClick={() => toggleTheme()}
-        >
-          🌓︎
-        </button>
-        <Link href="/rss.xml">
-          <Image
-            className="m-0"
-            src={rssIcon}
-            width={44}
-            height={44}
-            alt="rss icon"
-          />
-        </Link>
+    <nav className="border-b p-4">
+      <div className="flex max-w-screen-lg m-auto">
+        <div className="flex-auto">
+          <Link className="no-underline" href={'/'}>
+            <h1 className="leading-normal m-0">{title}</h1>
+          </Link>
+        </div>
+        <div className="flex flex-1 gap-x-2 items-center justify-end">
+          <button
+            type="button"
+            className="text-white bg-gray-300 hover:bg-gray-400 focus:ring-gray-500 focus:outline-none focus:ring-2 font-medium rounded-lg text-4xl px-1.5 pb-1 dark:bg-gray-100 dark:hover:dark:bg-gray-400 dark:focus:ring-gray-200"
+            onClick={() => toggleTheme()}
+          >
+            🌓︎
+          </button>
+          <Link href="/rss.xml">
+            <Image
+              alt="rss icon"
+              className="m-0"
+              height={44}
+              src={rssIcon}
+              style={{ maxWidth: '44px' }}
+              width={44}
+            />
+          </Link>
+          <Link
+            href="https://github.com/binarydiver/template-nextjs-blog"
+            target="_blank"
+          >
+            <Image
+              alt="github icon"
+              className="m-0"
+              height={44}
+              src={gitHubIcon}
+              style={{ maxWidth: '44px' }}
+              width={44}
+            />
+          </Link>
+        </div>
       </div>
     </nav>
   );
