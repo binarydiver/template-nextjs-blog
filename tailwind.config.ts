b62c9c42f97849973706e6af68ca894b693717d8
Jason Park <jason@binarydiver.com>
2024-09-19 15:11:10 +0900

    refactor: remove useless option for tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,11 @@
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 export default {
   content: ['./pages/**/*.{ts,tsx}'],
   darkMode: 'selector',
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Noto Sans KR', ...defaultTheme.fontFamily.sans],
-        serif: ['Noto Serif KR', ...defaultTheme.fontFamily.serif],
-      },
       typography: {
         DEFAULT: {
           css: {
